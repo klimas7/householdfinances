915cad58c192b191cb53d34c4a5f1fc2a767d8b7
klimas7 <klimas7@gmail.com>
2016-05-14 19:42:30 +0200

    Initial commit

---
+++ src/main/java/pl/klimas7/HouseholdfinancesApplication.java
@@ -0,0 +1,12 @@
+package pl.klimas7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdfinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdfinancesApplication.class, args);
+	}
+}
